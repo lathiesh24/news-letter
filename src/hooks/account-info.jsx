5b37b50a903f1fd68c41e10fe5d741d5207c3207
lathiesh24 <lathumahi24@gmail.com>
2023-08-04 22:59:43 +0530

    recent

--- src/hooks/account-info.jsx
+++ src/hooks/account-info.jsx
@@ -5,11 +5,15 @@ import { httpClient } from "../utils/http-client";
 const useAccountInfo = () => {
 
     useEffect(() => {
-        try {
 
-        } catch (error) {
-            console.error('Error fetching account info:', error);
+        const fetchAccountInfo = async () => {
+            try {
+                const response = await httpClient.get(``)
+            } catch (error) {
+                console.error('Error fetching account info:', error);
+            }
         }
+
 
     })
 
