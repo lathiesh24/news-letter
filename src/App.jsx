687231957b680cc033d36bdbbb23d6b33e92cb46
lathiesh24 <lathumahi24@gmail.com>
2023-08-07 13:24:06 +0530

    recent

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-
-import './App.css';
+import React from 'react';
 import Newsletter from './components/Newsletter';
+import useAccountInfo from './hooks/account-info';
 
 function App() {
+
+  const handleSubscribe = async (email) => {
+    console.log(email);
+  }
+
   return (
     <div className="App">
       <Newsletter />
